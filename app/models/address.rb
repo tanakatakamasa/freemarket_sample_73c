2e47cc2043950c10d157b638a539a04113268417
ryonozaki-tech <komakoma95515@gmail.com>
2020-04-25 17:54:50 +0900

    prefecture => prefecture_idにカラム変更

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,12 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  
+
   belongs_to :user
 
   with_options presence: true do
     validates :postal_code
-    validates :prefecture
+    validates :prefecture_id
     validates :city
     validates :street
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_071203) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.string "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "street", null: false
     t.string "apartment"
