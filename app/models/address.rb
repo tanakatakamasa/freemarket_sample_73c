234974802526d81af743a674192683aebfa81050
ryonozaki-tech <komakoma95515@gmail.com>
2020-04-25 17:22:59 +0900

    active_hashのための記述追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,7 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
   belongs_to :user
 
   with_options presence: true do
