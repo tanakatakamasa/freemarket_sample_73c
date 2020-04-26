4f2e023cb387fbf8aaff0f3512cbe1b367286484
knshst <62594253+knshst@users.noreply.github.com>
2020-04-26 15:12:27 +0900

    new_buyのルーティング 再修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :users
-  resources :items, only: [:index, :new, :show]
   get '/items/new_buy'
+  resources :items
 end
