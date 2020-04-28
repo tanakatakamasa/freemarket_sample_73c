24d1db5ca4604580d65433d1c1b2dafc983b0b52
waioli1223 <waioli1223@users.noreply.github.com>
2020-04-28 14:39:29 +0900

    先行/新規登録+ログイン機能
    ユーザー新規登録はできるがユーザー情報の登録と本人確認のみで通過できるようになっている。発送先住所情報の登録はが無くても登録できる様になっているため、後ほど修正する。
    ログイン画面は正常に動作している。
    仮置きでトップページにログアウトボタンを設置。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday])
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
 
   private
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -5,6 +5,10 @@ class Address < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
+    validates :family_name
+    validates :given_name
+    validates :family_name_kana
+    validates :given_name_kana
     validates :postal_code
     validates :prefecture_id
     validates :city
