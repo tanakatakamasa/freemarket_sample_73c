485f0f4b22c997ab0d4767b6278af4908247ad66
knshst <62594253+knshst@users.noreply.github.com>
2020-04-29 00:08:14 +0900

    各テーブルとマイグレーションファイル更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 7..128
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, password_length: 7..128
 
   has_one :address, dependent: :destroy
   has_one :card, dependent: :destroy
-  has_many :items, dependent: :destroy
-
+  has_many :buyer_items, class_name: 'Item', :foreign_key => 'buyer_id', dependent: :destroy
+  has_many :seller_items, class_name: 'Item', :foreign_key => 'seller_id', dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
 
--- db/migrate/20200425060510_create_addresses.rb
+++ db/migrate/20200425060510_create_addresses.rb
@@ -1,17 +1,17 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.string :last_name,          null: false
-      t.string :last_name_kana,     null: false
-      t.string :first_name,         null: false
-      t.string :first_name_kana,    null: false
-      t.string     :postal_code,   null: false
-      t.integer    :prefecture_id, null: false
-      t.string     :city,          null: false
-      t.string     :street,        null: false
+      t.string     :last_name,       null: false
+      t.string     :last_name_kana,  null: false
+      t.string     :first_name,      null: false
+      t.string     :first_name_kana, null: false
+      t.string     :postal_code,     null: false
+      t.integer    :prefecture_id,   null: false
+      t.string     :city,            null: false
+      t.string     :street,          null: false
       t.string     :apartment
       t.string     :phone_number
-      t.references :user,          foreign_key: true, null: false
+      t.references :user,            foreign_key: true, null: false
       t.timestamps
     end
   end
