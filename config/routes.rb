19b8c47519d3690279d71ec916de15d0bb1865ec
knshst <62594253+knshst@users.noreply.github.com>
2020-05-02 00:22:55 +0900

    クレジットカード登録・削除機能枠

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :users
-  # resources :items
-  #   resources :transactions
-
-  # resources :cards do
-  #   member do
-  #     get "confirm"
-  #     post "purchase"
-  #     get "done"
-  #   end
-  # end
-
+  resources :cards
   resources :items do
     resources :purchase, only: [:index] do
       # おそらくcollectionを使うのは、payもdoneもitemsのparamsが不要だから
