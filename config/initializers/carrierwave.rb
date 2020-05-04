7fb22d428a1da3c85be236a8d58987b22e66681c
uwkoichi <62323834+uwkoichi@users.noreply.github.com>
2020-05-04 19:58:51 +0900

    deploy検証

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'uwkoichi1'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uwkoich1'
+    config.asset_host = 'https://uwkoichi1.s3.amazonaws.com'
   end
 end
