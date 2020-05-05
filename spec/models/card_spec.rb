19109ca3590a0ec9dc1859b1f3049f73605b2ebf
knshst <62594253+knshst@users.noreply.github.com>
2020-05-05 22:40:12 +0900

    Update card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -9,13 +9,13 @@ describe Card do
     it "customer_idが空だと保存できない" do
       card = build(:card, customer_id: "")
       card.valid?
-      expect(card.errors[:customer_id]).to include("カード情報が正しくありません。")
+      expect(card.errors[:customer_id]).to include("を入力してください")
     end
 
     it "card_idが空だと保存できない" do
       card = build(:card, card_id: "")
       card.valid?
-      expect(card.errors[:card_id]).to include("カード情報が正しくありません。")
+      expect(card.errors[:card_id]).to include("を入力してください")
     end
 
   end
