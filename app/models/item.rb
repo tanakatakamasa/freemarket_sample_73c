5c3eed84a33d07be1fb22e27146798a876ccf854
uwkoichi <62323834+uwkoichi@users.noreply.github.com>
2020-05-05 12:28:17 +0900

    商品画像のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,20 +12,21 @@ class Item < ApplicationRecord
 
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-
+  # 画像がないと投稿できないようにするバリデーション
+  validates_presence_of :images 
 
   # helperオプションは出品機能が最低限完了し次第、付与。
   # 今はチームメンバーから一部引き継いでいる状態なので、要相談、一旦バリデーションはコメントアウトする
-  # with_options presence: true do
-  #   validates :name
-  #   validates :price
-  #   validates :discription
-  #   validates :category_id
-  #   validates :condition_id
-  #   validates :burden_id
-  #   validates :prefecture_id
-  #   validates :duration_id
-  # end
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :discription
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :duration_id
+  end
 
   # showアクションのビューで前の商品を呼び出すメソッド
   def previous
