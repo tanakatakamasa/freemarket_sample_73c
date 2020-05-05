4910434983a1d41ed3ba050d086f30408834e0e7
ryonozaki-tech <komakoma95515@gmail.com>
2020-05-05 17:30:03 +0900

    Update show.js

--- app/assets/javascripts/items/show.js
+++ app/assets/javascripts/items/show.js
@@ -2,7 +2,6 @@ $(function(){
   $(document).on('click', '.thumbImage li', function(){
     var class_name = $(this).attr("class");
     var number = class_name.slice(5);
-    // console.log(this);
     $('.mainImage li').hide();
     $('.image' + number).fadeIn();
   });
