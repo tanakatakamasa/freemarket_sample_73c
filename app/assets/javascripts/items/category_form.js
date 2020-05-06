705578f19d1e9f148e35482497ae8d1c74df3827
ryonozaki-tech <komakoma95515@gmail.com>
2020-05-06 18:59:31 +0900

    render :newのカテゴリー選択完了(引継ぎはできていない)

--- app/assets/javascripts/items/category_form.js
+++ app/assets/javascripts/items/category_form.js
@@ -1,94 +1,3 @@
-// $(function(){
-//   // カテゴリーセレクトボックスのオプションを作成
-//   function appendOption(category){
-//     var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
-//     return html;
-//   }
-//   // 子カテゴリーの表示作成
-//   function appendChidrenBox(insertHTML){
-//     var childSelectHtml = '';
-//     childSelectHtml = `<select class="in__main__show__categoryForm" id="child_category" name="">
-//                         <option value="選択してください" data-category="選択してください">選択してください</option>
-//                         ${insertHTML}
-//                       </select>`;
-//     $('.in__main__show__category').append(childSelectHtml);
-//   }
-//   // 孫カテゴリーの表示作成
-//   function appendGrandchidrenBox(insertHTML){
-//     var grandchildSelectHtml = '';
-//     grandchildSelectHtml = `<select class="in__main__show__categoryForm" id="grandchild_category" name="item[category_id]">
-//                               <option value="選択してください" data-category="選択してください">選択してください</option>
-//                               ${insertHTML}
-//                             </select>`;
-//     $('.in__main__show__category').append(grandchildSelectHtml);
-//   }
-//   // 親カテゴリー選択後のイベント
-//   $(document).on('change', '#parent_category', function(){
-//     var request = [
-//                 { url: 'get_category_children'},
-//                 { url: '/items/get_category_children'}
-//                 ];
-//     var jqXHRList = [];
-//     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
-//     if (parentCategory != "選択してください"){ //親カテゴリーが初期値でないことを確認
-//       for (var i = 0; i < request.length; i++) {
-//         jqXHRList.push(
-//         $.ajax({
-//           url: request[i].url,
-//           type: 'GET',
-//           data: { parent_name: parentCategory },
-//           dataType: 'json'
-//         }));
-//       .done(function(children){
-//         $('#child_category').remove(); //親が変更された時、子以下を削除するする
-//         $('#grandchild_category').remove();
-//         var insertHTML = '';
-//         children.forEach(function(child){
-//           insertHTML += appendOption(child);
-//         });
-//         appendChidrenBox(insertHTML);
-//       })
-//       .fail(function(){
-//         alert('カテゴリー取得に失敗しました');
-//       })
-//     }else{
-//       $('#child_category').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
-//       $('#grandchild_category').remove();
-//     }
-//   });
-//   // 子カテゴリー選択後のイベント
-//   $(document).on('change', '#child_category', function(){
-//     var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
-//     if (childId != "選択してください"){ //子カテゴリーが初期値でないことを確認
-//       $.ajax({
-//         url: 'get_category_grandchildren',
-//         type: 'GET',
-//         data: { child_id: childId },
-//         dataType: 'json'
-//       })
-//       .done(function(grandchildren){
-//         if (grandchildren.length != 0) {
-//           $('#grandchild_category').remove(); //子が変更された時、孫以下を削除するする
-//           var insertHTML = '';
-//           grandchildren.forEach(function(grandchild){
-//             insertHTML += appendOption(grandchild);
-//           });
-//           appendGrandchidrenBox(insertHTML);
-//         }
-//       })
-//       .fail(function(){
-//         alert('カテゴリー取得に失敗しました');
-//       })
-//     }else{
-//       $('#grandchild_category').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
-//     }
-//   });
-// });
-
-
-
-
-
 $(function(){
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category){
@@ -118,7 +27,7 @@ $(function(){
     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
     if (parentCategory != "選択してください"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_children',
+        url: '/items/get_category_children',
         type: 'GET',
         data: { parent_name: parentCategory },
         dataType: 'json'
@@ -145,7 +54,7 @@ $(function(){
     var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
     if (childId != "選択してください"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_grandchildren',
+        url: '/items/get_category_grandchildren',
         type: 'GET',
         data: { child_id: childId },
         dataType: 'json'
